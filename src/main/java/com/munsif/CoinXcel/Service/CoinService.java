823ef9dd78fde0b6a3b0b8054814b6585fbe9b02
Ahamed Munsif <113419042+munsif-dev@users.noreply.github.com>
2025-01-29 07:42:08 +0530

    build ok

--- src/main/java/com/munsif/CoinXcel/Service/CoinService.java
+++ src/main/java/com/munsif/CoinXcel/Service/CoinService.java
@@ -1,6 +1,6 @@
 package com.munsif.CoinXcel.Service;
-packare com.munsif.coinxcel.model.Coin;
+// packare com.munsif.coinxcel.model.Coin;
 
 public interface CoinService {
-    List<Coin>
+   // List<Coin>
 }
